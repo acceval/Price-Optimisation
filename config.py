6a051cdd12443830ee08c61ea372d0ce34cfda8e
Chandra Sutrisno Tjhong <chandra.sutrisno@gmail.com>
2022-04-06 08:42:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
 
 ## SERVER
-# PATH_TO_FOLDER = '/home/cst/Jixie/source code/unit_forecasting/'
 PATH_TO_FOLDER = str(os.getcwd())+'/'
-
 
 VERSION = '0.1.0'
 
 RANDOM_STATE = 42
-
-SL = 0.05
 
 # tree setting
 MAX_DEPTH=3
@@ -18,31 +14,43 @@ CCP_ALPHA=0.001
 
 
 LOCAL_URL = 'http://127.0.0.1:5050/'
-HEROKU_URL = 'https://price-segmentation.herokuapp.com/'
+HEROKU_URL = 'https://smarttradzt-price-optimisation.herokuapp.com/'
 
+ELASTICITY_CUTOFF = -0.3
+VOLUME_CONSTRAIN = 0.1
+
+
+# python main.py --env local --type B2c --filepath B2C_clean.csv --features Recency Revenue_L12 Customer_Size l3y_volume standard_cost l12_sales_vol Current_Price 
+# --price_feature Avg_Price_L3Y 
+# --volume_feature l3y_volume 
+# --product_feature Product_Group 
+# --current_price Current_Price 
+# --sales_volume l12_sales_vol 
+# --standard_cost standard_cost 
+# --segmentation_features Recency Revenue_L12 Customer_Size
 
 
 var = {}
 
 var['local'] = {}
-var['local']['filepath'] = 'sample_input_file.csv'
-var['local']['features'] = ['Customer_Type', 'Customer_Industry', 'Grade', 'Country', 'Destination_Port', 'City_State', 'Shipping_Condition', 'Export/Domestic', 'QUANTITY']
-var['local']['target_feature'] = 'Price_Premium'
-var['local']['index'] = 'Index'
-var['local']['price_per_segment'] = 'price_per_segment.json'
-var['local']['global_threshold'] = 'global_threshold.json'
-var['local']['customised_threshold'] = 'customised_threshold_final.json'
-var['local']['price_power_index_threshold']	 = 'price_power_index_final.json'
-var['local']['bad_price_per_segment'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/bad_price_per_segment.json'
+var['local']['filepath'] = 'B2C_clean.csv'
+var['local']['features'] = ['Recency','Revenue_L12','Customer_Size','l3y_volume','standard_cost','l12_sales_vol','Current_Price']
+var['local']['price_feature'] = 'Avg_Price_L3Y'
+var['local']['volume_feature'] = 'l3y_volume'
+var['local']['product_feature'] = 'Product_Group'
+var['local']['current_price'] = 'Current_Price'
+var['local']['sales_volume'] = 'l12_sales_vol'
+var['local']['standard_cost'] = 'standard_cost'
+var['local']['segmentation_features'] = ['Recency','Revenue_L12','Customer_Size']
 
 
 var['prod'] = {}
-var['prod']['filepath'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/sample_input_file.csv'
-var['prod']['features'] = ['Customer_Type', 'Customer_Industry', 'Grade', 'Country', 'Destination_Port', 'City_State', 'Shipping_Condition', 'Export/Domestic', 'QUANTITY']
-var['prod']['target_feature'] = 'Price_Premium'
-var['prod']['index'] = 'Index'
-var['prod']['price_per_segment'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/price_per_segment.json'
-var['prod']['global_threshold'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/global_threshold.json'
-var['prod']['customised_threshold'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/customised_threshold_final.json'
-var['prod']['price_power_index_threshold']	 = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/price_power_index_final.json'
-var['prod']['bad_price_per_segment'] = 'https://raw.githubusercontent.com/acceval/Price-Segmentation/main/bad_price_per_segment.json'
+var['prod']['filepath'] = 'https://raw.githubusercontent.com/acceval/Price-Optimisation/main/B2C_clean.csv'
+var['prod']['features'] = ['Recency','Revenue_L12','Customer_Size','l3y_volume','standard_cost','l12_sales_vol','Current_Price']
+var['prod']['price_feature'] = 'Avg_Price_L3Y'
+var['prod']['volume_feature'] = 'l3y_volume'
+var['prod']['product_feature'] = 'Product_Group'
+var['prod']['current_price'] = 'Current_Price'
+var['prod']['sales_volume'] = 'l12_sales_vol'
+var['prod']['standard_cost'] = 'standard_cost'
+var['prod']['segmentation_features'] = ['Recency','Revenue_L12','Customer_Size']
